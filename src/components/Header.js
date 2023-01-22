6f5ab7939ecba94c3ce162ec1993538916f1fac2
david-walters <dawudwalters@gmail.com>
2023-01-22 11:16:31 +0000

    Utilised the navbar images and applied the style names for the css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,34 @@
+import styles from "../style.module.css";
+import logo from "../images/logo.svg"
+import cart_icon from "../images/icon-cart.svg"
+import avatar from "../images/image-avatar.png"
+
 const Header = () => {
-    return <div></div>
+    return (<div>
+<nav>
+  <div>
+    <img className={styles.logo} src={logo} alt="Logo"/>
+  </div>
+  
+  <ul>
+    <li><a href="#">Collections</a></li>
+    <li><a href="#">Men</a></li>
+    <li><a href="#">Women</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+
+  <div className={styles.icons}>
+    <a href="#">
+        <img src={cart_icon} alt="Shopping Cart"/>
+    </a>
+    <a href="#">
+        <img src={avatar} className={styles.avatar} alt="Avatar"/>
+    </a>
+  </div>
+
+</nav>
+        </div>)
 }
 
 export default Header
