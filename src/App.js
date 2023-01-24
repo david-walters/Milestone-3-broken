958bd68623dec861e77a97502141d6a60e8d9a65
david-walters <dawudwalters@gmail.com>
2023-01-24 20:34:34 +0000

    Imported the ProductImage component and added it to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import ProductImage from "./components/ProductImage";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <ProductImage></ProductImage>
     </div>
   );
 }
