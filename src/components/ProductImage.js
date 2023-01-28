7bed444ab9365aa902ddfa7e69d3ff7b0012af72
david-walters <dawudwalters@gmail.com>
2023-01-28 19:36:25 +0000

    Imported the ProductInfo component into the ProductImage component so I was able to use Grid and place it next to the images and not underneath them

--- src/components/ProductImage.js
+++ src/components/ProductImage.js
@@ -4,6 +4,7 @@ import shoes1 from '../images/image-product-1.jpg'
 import shoes2 from '../images/image-product-2.jpg'
 import shoes3 from '../images/image-product-3.jpg'
 import shoes4 from '../images/image-product-4.jpg'
+import ProductInfo from './ProductInfo'
 
 const shoes = [shoes1, shoes2, shoes3, shoes4,]
 
@@ -11,20 +12,22 @@ function ProductImage() {
   const [currentImage, setCurrentImage] = useState(shoes[0]);
 
   return (
+  <div className={styles.wrapper}>
     <div className={styles.productImage}>
       <img src={currentImage} alt="Product" className={styles.mainImage}/>
-      <div className={styles.thumbnailContainer}>
-        {shoes.map((image, index) => (
-          <img
+        <div className={styles.thumbnailContainer}>
+            {shoes.map((image, index) => (
+            <img
             key={index}
             src={image}
             alt="Thumbnail"
             className={`${styles.thumbnail} ${currentImage === image ? `${styles.selected}` : ""}`}
-            onClick={() => setCurrentImage(image)}
-          />
-        ))}
-      </div>
+            onClick={() => setCurrentImage(image)} />
+             ))}
+        </div>
     </div>
+      <div className={styles.productInfo}><ProductInfo></ProductInfo></div>
+  </div>
   );
 }
 
