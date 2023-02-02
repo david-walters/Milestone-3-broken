65ff0b1a40536b5c66ab34322caa21d865bc0da0
david-walters <dawudwalters@gmail.com>
2023-02-02 23:30:38 +0000

    I moved the usestate variables to the App component to make them accessible for other components via props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, { useState } from 'react';
 import Header from "./components/Header";
 import ProductImage from "./components/ProductImage";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [itemTotal, setItemTotal] = useState(0)
+
+
+
   return (
     <div className="App">
-      <Header></Header>
-      <ProductImage></ProductImage>
+      <Header count={count}
+              setCount={setCount}
+              itemTotal={itemTotal}
+              setItemTotal={setItemTotal}/>
+
+      <ProductImage count={count}
+                    setCount={setCount}
+                    itemTotal={itemTotal}
+                    setItemTotal={setItemTotal}/>
     </div>
   );
 }
