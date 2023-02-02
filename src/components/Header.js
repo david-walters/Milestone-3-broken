0d9d802f4825c1b71a145b3cb6e22aca9b5d6929
david-walters <dawudwalters@gmail.com>
2023-02-02 23:40:30 +0000

    I inserted the Cart component into the header so that it appears like a dropdown menu when the user hovers over the cart icon. I also added the span that will reveal how many items are in the cart before hovering over it

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,9 @@ import styles from "../style.module.css";
 import logo from "../images/logo.svg"
 import cart_icon from "../images/icon-cart.svg"
 import avatar from "../images/image-avatar.png"
+import Cart from "./Cart";
 
-const Header = () => {
+const Header = ({count, setCount, itemTotal, setItemTotal}) => {
     return (<div>
 <nav>
   <div>
@@ -19,9 +20,19 @@ const Header = () => {
   </ul>
 
   <div className={styles.icons}>
-    <a href="#">
+    <a className={styles.cartIcon} href="#">
         <img className={styles.icon1} src={cart_icon} alt="Shopping Cart"/>
+        {itemTotal > 0 && <span className={styles.itemCount}>{itemTotal}</span>}
     </a>
+        <div className={styles.cartContainer}>
+          <div className={styles.cartTitle}>Cart</div>
+          <div className={styles.cartContent}>
+            <Cart count={count}
+                  setCount={setCount}
+                  itemTotal={itemTotal}
+                  setItemTotal={setItemTotal}/>
+          </div>
+        </div>
     <a href="#">
         <img src={avatar} className={styles.avatar} alt="Avatar"/>
     </a>
