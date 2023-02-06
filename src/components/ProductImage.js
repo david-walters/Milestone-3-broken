438767e868b7e3cd3ed5946abb7c7283ba72864d
david-walters <dawudwalters@gmail.com>
2023-02-06 21:42:15 +0000

    Added an onClick function to the main image which sets the showMoadal to True, which then presents the Modal component.

--- src/components/ProductImage.js
+++ src/components/ProductImage.js
@@ -5,24 +5,35 @@ import shoes2 from '../images/image-product-2.jpg'
 import shoes3 from '../images/image-product-3.jpg'
 import shoes4 from '../images/image-product-4.jpg'
 import ProductInfo from './ProductInfo'
+import Modal from './Modal'
+import shortid from 'shortid'
 
 const shoes = [shoes1, shoes2, shoes3, shoes4,]
 
 function ProductImage({count, setCount, itemTotal, setItemTotal}) {
+  
   const [currentImage, setCurrentImage] = useState(shoes[0]);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = (img) => {
+    setCurrentImage(img);
+    setShowModal(true);
+  };
+
 
   return (
   <div className={styles.wrapper}>
     <div className={styles.productImage}>
-      <img src={currentImage} alt="Product" className={styles.mainImage}/>
+      <img src={currentImage} alt="Product" className={styles.mainImage} onClick={() => handleShowModal(currentImage)}/>
         <div className={styles.thumbnailContainer}>
-            {shoes.map((image, index) => (
+            {shoes.map((image) => (
+        <div className={`${styles.thumbnailDiv} ${currentImage === image ? `${styles.selectedDiv}` : ""}`} key={shortid.generate()}>
           <img
-            key={index}
             src={image}
             alt="Thumbnail"
             className={`${styles.thumbnail} ${currentImage === image ? `${styles.selected}` : ""}`}
-            onClick={() => setCurrentImage(image)} />
+            onClick={() => setCurrentImage(image)}/>
+        </div>
              ))}
         </div>
     </div>
@@ -32,6 +43,16 @@ function ProductImage({count, setCount, itemTotal, setItemTotal}) {
                                             itemTotal={itemTotal}
                                             setItemTotal={setItemTotal}/>
       </div>
+
+      <div>
+            {showModal && (
+              <Modal shoes={shoes}
+              setShowModal={setShowModal}
+              currentImage={currentImage}
+              setCurrentImage={setCurrentImage}/>
+            )}
+      </div>
+      
   </div>
   );
 }
