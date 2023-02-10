8c8fc7a672c57298a9b6c9903eba9ce48eee8c22
david-walters <dawudwalters@gmail.com>
2023-02-10 10:35:18 +0000

    I added the use effect hook to change the text of a p tag when the screen width renders to mobile. I noticed the p tag in the image provided for the challenge was different in the mobile view than in desktop view. It called the item Autumn limited edition as opposed to Fall limited edition. I beleieve this is a mistake, but nevertheless, I still made this code to replicate the image

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,23 @@
+import { useState, useEffect } from "react";
 import styles from "../style.module.css";
 import productImage from "../images/image-product-1.jpg";
 import bin from "../images/icon-delete.svg";
 
 function Cart ({itemTotal, setItemTotal}) {
+
+    const [text, setText] = useState('Fall Limited Edition Sneakers');
+  
+    useEffect(() => {
+      if (window.innerWidth <= 768) {
+        setText('Autumn Limited Edition...');
+      }
+    }, []);
+  
+    
     const reset = () => {
         setItemTotal(0)
     }
+    
     return (
         <>
             {itemTotal === 0 ? (
@@ -14,7 +26,8 @@ function Cart ({itemTotal, setItemTotal}) {
                 <>
                     <div className={styles.cartItem}>
                         <img src={productImage} className={styles.shoeImage} alt="Fall Limited Edition Sneakers"/>
-                        <p>Fall Limited Edition Sneakers $125.00 x {itemTotal} <span>${(125.00 * itemTotal).toFixed(2)}</span></p>
+                        <p>{text}</p>
+                        <p>$125.00 x {itemTotal} <span>${(125.00 * itemTotal).toFixed(2)}</span></p>
                         <img src={bin} className={styles.bin} alt="delete" onClick={reset}/> 
                         <button className={styles.checkoutButton}>Checkout</button>
                     </div>
